7d9690a5c9b13be566451ff1b13dfbfa4c63a0c3
CrispyCroissant <gentlemug00@gmail.com>
2022-02-05 12:48:26 +0100

    Created "multithreaded" wikipedia fetcher

---
+++ backend/src/utils/worker.js
@@ -0,0 +1,59 @@
+const axios = require("axios");
+const { parentPort } = require("worker_threads");
+const { getArticleDates } = require("./article");
+
+const wikipediaURL = "https://en.wikipedia.org/w/api.php";
+let articles = [];
+
+axios
+  .get(wikipediaURL, {
+    params: {
+      format: "json",
+      action: "query",
+      generator: "random",
+      grnlimit: 20,
+      grnnamespace: 0,
+      grnfilterredir: "nonredirects",
+      prop: "description|pageimages|wbentityusage|info",
+      piprop: "thumbnail",
+      pithumbsize: 10000,
+      pilicense: "any",
+      wbeuaspect: "T",
+      inprop: "url",
+    },
+  })
+  .then(async (res) => {
+    const data = res.data.query.pages;
+
+    /*
+     * Check if the random article has a description, thumbnail, and a WikiData
+     * entry.
+     */
+    for (let i = 0; i < Object.keys(data).length; i++) {
+      const article = data[Object.keys(data)[i]];
+
+      const desc = article.description;
+      const thumbnail = article.thumbnail ? article.thumbnail.source : null;
+      const wikibaseItem = article.wbentityusage
+        ? Object.keys(article.wbentityusage)[0]
+        : null;
+
+      if (!desc || !thumbnail || !wikibaseItem) {
+        continue;
+      }
+
+      articles.push({
+        title: article.title,
+        desc,
+        thumbnail,
+        url: article.fullurl,
+        wikibaseItem,
+      });
+
+      articles = await getArticleDates(articles);
+
+      if (articles.length !== 0) {
+        parentPort.postMessage(articles[0]);
+      }
+    }
+  });
