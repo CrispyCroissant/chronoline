bffea59889e3effd6b4d371b7060191eb75f3e43
CrispyCroissant <gentlemug00@gmail.com>
2022-02-07 15:05:02 +0100

    Refactored worker

--- backend/src/utils/worker.js
+++ backend/src/utils/worker.js
@@ -2,70 +2,68 @@ const axios = require("axios");
 const { parentPort } = require("worker_threads");
 const { getArticleDates } = require("./article");
 
-const wikipediaURL = "https://en.wikipedia.org/w/api.php";
-const articlesPerRequest = 80;
-let articles = [];
+generateCard();
 
-doWork();
+async function generateCard() {
+  const wikipediaURL = "https://en.wikipedia.org/w/api.php";
+  const articlesPerRequest = 80;
+  let articles = [];
 
-function doWork() {
-  axios
-    .get(wikipediaURL, {
-      params: {
-        format: "json",
-        action: "query",
-        generator: "random",
-        grnlimit: articlesPerRequest,
-        grnnamespace: 0,
-        grnfilterredir: "nonredirects",
-        prop: "description|pageimages|wbentityusage|info",
-        piprop: "thumbnail",
-        pithumbsize: 10000,
-        pilicense: "any",
-        wbeuaspect: "T",
-        inprop: "url",
-      },
-      headers: {
-        "Accept-Encoding": "gzip",
-      },
-    })
-    .then(async (res) => {
-      const data = res.data.query.pages;
+  const res = await axios.get(wikipediaURL, {
+    params: {
+      format: "json",
+      action: "query",
+      generator: "random",
+      grnlimit: articlesPerRequest,
+      grnnamespace: 0,
+      grnfilterredir: "nonredirects",
+      prop: "description|pageimages|wbentityusage|info",
+      piprop: "thumbnail",
+      pithumbsize: 10000,
+      pilicense: "any",
+      wbeuaspect: "T",
+      inprop: "url",
+    },
+    headers: {
+      "Accept-Encoding": "gzip",
+    },
+  });
 
-      /*
-       * Check if the random article has a description, thumbnail, and a WikiData
-       * entry.
-       */
-      for (let i = 0; i < Object.keys(data).length; i++) {
-        const article = data[Object.keys(data)[i]];
+  const data = res.data.query.pages;
 
-        const desc = article.description;
-        const thumbnail = article.thumbnail ? article.thumbnail.source : null;
-        const wikibaseItem = article.wbentityusage
-          ? Object.keys(article.wbentityusage)[0]
-          : null;
+  /*
+   * Check if the random article has a description, thumbnail, and a WikiData
+   * entry.
+   */
+  for (let i = 0; i < Object.keys(data).length; i++) {
+    const article = data[Object.keys(data)[i]];
 
-        if (!desc || !thumbnail || !wikibaseItem) {
-          if (i === articlesPerRequest - 1) {
-            doWork();
-          }
-          continue;
-        }
+    const desc = article.description;
+    const thumbnail = article.thumbnail ? article.thumbnail.source : null;
+    const wikibaseItem = article.wbentityusage
+      ? Object.keys(article.wbentityusage)[0]
+      : null;
 
-        articles.push({
-          title: article.title,
-          desc,
-          thumbnail,
-          url: article.fullurl,
-          wikibaseItem,
-        });
-
-        articles = await getArticleDates(articles);
-
-        if (articles.length !== 0) {
-          parentPort.postMessage(articles[0]);
-          return;
-        }
+    if (!desc || !thumbnail || !wikibaseItem) {
+      if (i === articlesPerRequest - 1) {
+        await generateCard();
       }
+      continue;
+    }
+
+    articles.push({
+      title: article.title,
+      desc,
+      thumbnail,
+      url: article.fullurl,
+      wikibaseItem,
     });
+
+    articles = await getArticleDates(articles);
+
+    if (articles.length !== 0) {
+      parentPort.postMessage(articles[0]);
+      return;
+    }
+  }
 }
