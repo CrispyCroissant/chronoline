9d440cb24180c82f5571b5492d41d8a778c61a8e
CrispyCroissant <48359983+CrispyCroissant@users.noreply.github.com>
2022-02-08 10:37:10 +0000

    [CH-9] Create footer (#5)
    * Created a footer component
    * Added meta tags to all routes which is used to check if a page should render the footer component.

---
+++ frontend/src/components/nav/TheFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-footer color="secondary">
+    <v-row justify="center" class="ma-0">
+      <p class="text-overline white--text mb-0 py-3">
+        Game made by
+        <a
+          class="font-weight-black grey--text text--darken-3"
+          href="https://github.com/CrispyCroissant/"
+        >
+          @CrispyCroissant
+        </a>
+      </p>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  background: #e94f37;
+}
+</style>
