2c42d760f0dd31a74b76aa93b846b8f9fd232125
CrispyCroissant <gentlemug00@gmail.com>
2022-02-09 19:54:13 +0100

    Created the table/playing area

---
+++ frontend/src/components/PlayGame/TheTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-sheet class="pa-10" color="transparent" width="100%">
+    <hr />
+    <v-row class="my-7">
+      <PlayingCard class="mx-2" />
+      <PlayingCard class="mx-2" />
+      <PlayingCard class="mx-2" />
+      <PlayingCard class="mx-2" />
+      <PlayingCard class="mx-2" />
+    </v-row>
+    <hr />
+  </v-sheet>
+</template>
+
+<script>
+import PlayingCard from "./PlayingCard.vue";
+
+export default {
+  components: { PlayingCard },
+  name: "TheTable",
+};
+</script>
+
+<style></style>
