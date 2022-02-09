5671e8ecdae788486f544b37fb3cd1ec26bd9e9a
CrispyCroissant <gentlemug00@gmail.com>
2022-02-09 14:21:38 +0100

    Created player menu

---
+++ frontend/src/components/PlayGame/MenuPlayers.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p class="text-body-1">Look at the other player's cards</p>
+    <v-btn-toggle class="d-flex flex-column" mandatory>
+      <v-btn color="secondary" max-width="10rem">Player 1</v-btn>
+      <v-btn color="secondary" max-width="10rem">Player 2</v-btn>
+      <v-btn color="secondary" max-width="10rem">Player 3</v-btn>
+    </v-btn-toggle>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
