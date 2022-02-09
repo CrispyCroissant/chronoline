6332de5deb7fc9e1038fd92c77ebe786ce5f1728
CrispyCroissant <gentlemug00@gmail.com>
2022-02-09 20:04:24 +0100

    Changed table design

--- frontend/src/components/PlayGame/TheTable.vue
+++ frontend/src/components/PlayGame/TheTable.vue
@@ -1,14 +1,18 @@
 <template>
-  <v-sheet class="pa-10" color="transparent" width="100%">
-    <hr />
-    <v-row class="my-7">
+  <v-sheet
+    class="px-10 py-16"
+    color="onyx"
+    elevation="4"
+    rounded="xl"
+    width="98%"
+  >
+    <v-row>
       <PlayingCard class="mx-2" />
       <PlayingCard class="mx-2" />
       <PlayingCard class="mx-2" />
       <PlayingCard class="mx-2" />
       <PlayingCard class="mx-2" />
     </v-row>
-    <hr />
   </v-sheet>
 </template>
 
