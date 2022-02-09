641abb2cf31fd864d4fd8cb25fd982100743389b
CrispyCroissant <gentlemug00@gmail.com>
2022-02-09 14:21:58 +0100

    Added the components to the page

--- frontend/src/views/PlayGame.vue
+++ frontend/src/views/PlayGame.vue
@@ -1,11 +1,17 @@
 <template>
   <v-container fluid fill-height>
-    <h1>Placeholder</h1>
+    <v-row justify="center"></v-row>
+    <v-row justify="center">
+      <PlayerCardSheet />
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import PlayerCardSheet from "../components/PlayGame/PlayerCardSheet.vue";
+
 export default {
+  components: { PlayerCardSheet },
   name: "PlayGame",
 };
 </script>
