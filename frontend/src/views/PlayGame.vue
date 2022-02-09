589640b8495f01840b59084a70759e02c025c97b
CrispyCroissant <gentlemug00@gmail.com>
2022-02-09 19:54:24 +0100

    Added a title to the page and the table component

--- frontend/src/views/PlayGame.vue
+++ frontend/src/views/PlayGame.vue
@@ -1,8 +1,14 @@
 <template>
   <v-container fluid class="d-flex flex-column">
     <TheDialog />
-    <v-row justify="end" align="start">
+    <v-row justify="space-between" align="center">
+      <v-row justify="start" class="ml-16">
+        <h3 class="text-h2">Chronoline</h3>
+      </v-row>
       <TheScoreboard />
+    </v-row>
+    <v-row justify="center" align="center">
+      <TheTable />
     </v-row>
     <v-row justify="center" align="end">
       <ThePlayerCardSheet />
@@ -14,9 +20,10 @@
 import TheDialog from "../components/PlayGame/TheDialog.vue";
 import ThePlayerCardSheet from "../components/PlayGame/ThePlayerCardSheet.vue";
 import TheScoreboard from "../components/PlayGame/TheScoreboard.vue";
+import TheTable from "../components/PlayGame/TheTable.vue";
 
 export default {
-  components: { TheScoreboard, ThePlayerCardSheet, TheDialog },
+  components: { TheScoreboard, ThePlayerCardSheet, TheDialog, TheTable },
   name: "PlayGame",
 };
 </script>
