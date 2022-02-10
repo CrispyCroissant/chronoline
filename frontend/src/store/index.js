caaeb305efb263240576195ab07adccf1723ee50
CrispyCroissant <gentlemug00@gmail.com>
2022-02-10 12:13:27 +0100

    Added new states and mutations

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,12 +3,37 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+/*
+ * Structure of the state.
+ *
+ * cardDeck: [Card]
+ * cardsOnTable: [Card]
+ * players: [
+ *  {
+ *    nickname: "",
+ *    cards: [Card],
+ *    cardAmount: cards.length
+ *  }
+ * ]
+ */
 export const state = {
   nickname: "",
+  cardDeck: [],
+  cardsOnTable: [],
+  players: [],
 };
 export const mutations = {
   setNickname(state, nickname) {
     state.nickname = nickname;
+  },
+  setCardDeck(state, deck) {
+    state.cardDeck = deck;
+  },
+  setCardsOnTable(state, cards) {
+    state.cardsOnTable = cards;
+  },
+  setPlayers(state, players) {
+    state.players = players;
   },
 };
 export const actions = {};
--- frontend/tests/unit/store/index.spec.js
+++ frontend/tests/unit/store/index.spec.js
@@ -4,6 +4,18 @@ describe("The store", () => {
   it("has a nickname state", () => {
     expect(state.nickname).toBeDefined();
   });
+
+  it("has a card deck state", () => {
+    expect(state.cardDeck).toBeDefined();
+  });
+
+  it("has a cards on table/played cards state", () => {
+    expect(state.cardsOnTable).toBeDefined();
+  });
+
+  it("has a players state", () => {
+    expect(state.players).toBeDefined();
+  });
 });
 
 describe("The mutations", () => {
@@ -11,5 +23,38 @@ describe("The mutations", () => {
     mutations.setNickname(state, "test");
 
     expect(state.nickname).toBe("test");
+  });
+
+  it("can set the card deck", () => {
+    const payload = [{ title: "test", desc: "test" }];
+
+    mutations.setCardDeck(state, payload);
+
+    expect(state.cardDeck).toEqual(payload);
+  });
+
+  it("can set the cards on the table", () => {
+    const payload = [{ title: "test", desc: "test" }];
+
+    mutations.setCardsOnTable(state, payload);
+
+    expect(state.cardsOnTable).toEqual(payload);
+  });
+
+  it("can set the players array", () => {
+    const payload = [
+      {
+        nickname: "User",
+        cards: [{ title: "test", desc: "test" }],
+        cardAmount() {
+          return this.cards.length;
+        },
+      },
+    ];
+
+    mutations.setPlayers(state, payload);
+
+    expect(state.players).toEqual(payload);
+    expect(state.players[0].cardAmount()).toEqual(1);
   });
 });
