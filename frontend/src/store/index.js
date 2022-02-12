320db1c6d79a7dcf0b725900787c4c09f1e9e70a
CrispyCroissant <48359983+CrispyCroissant@users.noreply.github.com>
2022-02-12 12:22:49 +0000

    [CH-13] Flesh out the Vuex store (#8)
    * States with corresponding mutations.
      * Added a card deck state.
      * Added a table state.
      * Added a "players" state. 
      * Added a current turn state.
    * Getters
      * Added a getter which returns the players in order of least cards.
      * Added a getter which returns a specific player's cards. 
    * Actions
      * Added a store initialization method which will be used upon game initialization with data from the backend.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,19 +3,69 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+/*
+ * Structure of the state.
+ *
+ * cardDeck: [Card]
+ * cardsOnTable: [Card]
+ * players: [
+ *  {
+ *    nickname: "",
+ *    cards: [Card],
+ *    cardAmount: cards.length
+ *  }
+ * ]
+ */
 export const state = {
   nickname: "",
+  cardDeck: [],
+  cardsOnTable: [],
+  players: [],
+  currentPlayerTurn: "",
 };
 export const mutations = {
   setNickname(state, nickname) {
     state.nickname = nickname;
   },
+  setCardDeck(state, deck) {
+    state.cardDeck = deck;
+  },
+  setCardsOnTable(state, cards) {
+    state.cardsOnTable = cards;
+  },
+  setPlayers(state, players) {
+    state.players = players;
+  },
+  setCurrentTurn(state, player) {
+    state.currentPlayerTurn = player;
+  },
 };
-export const actions = {};
+export const actions = {
+  initStore({ commit }, data) {
+    commit("setCardDeck", data.deck);
+    commit("setCardsOnTable", data.table);
+    commit("setPlayers", data.players);
+    commit("setCurrentTurn", data.currentTurn);
+  },
+};
+export const getters = {
+  playerLeaderboard(state) {
+    return state.players.sort((a, b) => {
+      return a.cardAmount - b.cardAmount;
+    });
+  },
+  playersCards(state, name) {
+    const player = state.players.find((player) => {
+      return player.nickname === name;
+    });
+    return player.cards;
+  },
+};
 
 export default new Vuex.Store({
   state,
   mutations,
   actions,
+  getters,
   modules: {},
 });
--- frontend/tests/unit/store/index.spec.js
+++ frontend/tests/unit/store/index.spec.js
@@ -1,15 +1,136 @@
-import { state, mutations } from "../../../src/store";
+import { state, mutations, getters, actions } from "../../../src/store";
 
 describe("The store", () => {
   it("has a nickname state", () => {
     expect(state.nickname).toBeDefined();
   });
+
+  it("has a card deck state", () => {
+    expect(state.cardDeck).toBeDefined();
+  });
+
+  it("has a cards on table/played cards state", () => {
+    expect(state.cardsOnTable).toBeDefined();
+  });
+
+  it("has a players state", () => {
+    expect(state.players).toBeDefined();
+  });
+
+  it("has a current player's turn state", () => {
+    expect(state.currentPlayerTurn).toBeDefined();
+  });
 });
 
 describe("The mutations", () => {
-  it("can set the nickname state", () => {
-    mutations.setNickname(state, "test");
+  describe("setNickname", () => {
+    it("can set the nickname state", () => {
+      mutations.setNickname(state, "test");
 
-    expect(state.nickname).toBe("test");
+      expect(state.nickname).toBe("test");
+    });
+  });
+
+  describe("setCardDeck", () => {
+    it("can set the card deck", () => {
+      const payload = [{ title: "test", desc: "test" }];
+
+      mutations.setCardDeck(state, payload);
+
+      expect(state.cardDeck).toEqual(payload);
+    });
+  });
+
+  describe("setCardsOnTable", () => {
+    it("can set the cards on the table", () => {
+      const payload = [{ title: "test", desc: "test" }];
+
+      mutations.setCardsOnTable(state, payload);
+
+      expect(state.cardsOnTable).toEqual(payload);
+    });
+  });
+
+  describe("setPlayers", () => {
+    it("can set the players array", () => {
+      const payload = [
+        {
+          nickname: "User",
+          cards: [{ title: "test", desc: "test" }],
+          cardAmount() {
+            return this.cards.length;
+          },
+        },
+      ];
+
+      mutations.setPlayers(state, payload);
+
+      expect(state.players).toEqual(payload);
+      expect(state.players[0].cardAmount()).toEqual(1);
+    });
+  });
+
+  describe("setCurrentTurn", () => {
+    it("can set the current turn to be a specific player's", () => {
+      mutations.setCurrentTurn(state, "TestPlayer");
+
+      expect(state.currentPlayerTurn).toEqual("TestPlayer");
+    });
+  });
+});
+
+describe("The actions", () => {
+  describe("initStore", () => {
+    it("initializes the store with the given data", () => {
+      const table = ["Card1", "Card2"];
+      const deck = ["Card3", "Card4"];
+      const players = ["Player1", "Player2"];
+      const currentTurn = "Player1";
+
+      const data = { table, deck, players, currentTurn };
+
+      const commit = (mutationName, payload) => {
+        mutations[mutationName](state, payload);
+      };
+      actions.initStore({ commit }, data);
+
+      expect(state.cardsOnTable).toEqual(table);
+      expect(state.cardDeck).toEqual(deck);
+      expect(state.players).toEqual(players);
+      expect(state.currentPlayerTurn).toBe(currentTurn);
+    });
+  });
+});
+
+describe("The getters", () => {
+  describe("playerLeaderboard", () => {
+    it("can sort the players by least amount of cards", () => {
+      const mockedState = {
+        players: [{ cardAmount: 3 }, { cardAmount: 6 }, { cardAmount: 2 }],
+      };
+
+      const result = getters.playerLeaderboard(mockedState);
+
+      expect(result).toEqual([
+        { cardAmount: 2 },
+        { cardAmount: 3 },
+        { cardAmount: 6 },
+      ]);
+    });
+  });
+
+  describe("playersCards", () => {
+    it("can return a certain player's cards", () => {
+      const mockedState = {
+        players: [
+          { nickname: "RealPlayer", cards: [] },
+          { nickname: "TestPlayer", cards: ["TestCard"] },
+        ],
+      };
+
+      const result = getters.playersCards(mockedState, "TestPlayer");
+
+      expect(result).toEqual(["TestCard"]);
+    });
   });
 });
