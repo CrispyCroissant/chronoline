93d9fce2cda06211f9fd006bc5a32252654715d1
CrispyCroissant <gentlemug00@gmail.com>
2022-02-18 17:33:11 +0100

    Fixed broken texts

--- frontend/src/components/PlayGame/ThePlayersMenu.vue
+++ frontend/src/components/PlayGame/ThePlayersMenu.vue
@@ -1,20 +1,30 @@
 <template>
   <div>
-    <p class="text-caption text-center mr-2">Look at other players</p>
-    <v-btn-toggle class="d-flex flex-column" mandatory dense shaped borderless>
-      <v-btn
-        :color="color(player.nickname)"
-        max-width="10rem"
-        height="2rem"
-        v-for="player in players"
-        :key="player.nickname"
-        @click="changePlayer(player.nickname)"
+    <v-row justify="center">
+      <p class="text-caption">Look at other players</p>
+    </v-row>
+    <v-row class="d-flex flex-column align-center">
+      <v-btn-toggle
+        class="d-flex flex-column"
+        style="width: 10rem"
+        mandatory
+        dense
+        shaped
+        borderless
       >
-        {{
-          player.nickname === $store.state.nickname ? "You" : player.nickname
-        }}
-      </v-btn>
-    </v-btn-toggle>
+        <v-btn
+          :color="color(player.nickname)"
+          height="2rem"
+          v-for="player in players"
+          :key="player.nickname"
+          @click="changePlayer(player.nickname)"
+        >
+          {{
+            player.nickname === $store.state.nickname ? "You" : player.nickname
+          }}
+        </v-btn>
+      </v-btn-toggle>
+    </v-row>
   </div>
 </template>
 
