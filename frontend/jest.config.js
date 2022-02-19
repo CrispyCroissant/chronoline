0889b6f6c0af902ad49f6be24d9bc85b0a7b02db
CrispyCroissant <gentlemug00@gmail.com>
2022-02-19 18:17:36 +0100

    Fixed broken tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   setupFiles: ["<rootDir>/tests/unit/setup.js"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/tests/__mock__/fileMock.js",
+  },
 };
