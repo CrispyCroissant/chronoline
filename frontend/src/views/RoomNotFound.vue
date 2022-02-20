e682aef712f614b06048f73fe47e38ce49ae76ca
CrispyCroissant <gentlemug00@gmail.com>
2022-02-20 13:43:08 +0100

    Made page into a card. Improved design

--- frontend/src/views/RoomNotFound.vue
+++ frontend/src/views/RoomNotFound.vue
@@ -1,21 +1,29 @@
 <template>
-  <v-container fluid class="d-flex flex-column">
-    <v-row justify="center">
-      <v-icon color="primary" aria-hidden="true">
-        mdi-alert-circle-outline
-      </v-icon>
-    </v-row>
-    <v-row justify="center" align="center">
-      <h1 class="text-h1 onyx--text">Sorry,</h1>
-    </v-row>
-    <v-row justify="center" align="start">
-      <p class="text-h3 text-center onyx--text">that room doesn't exist!</p>
-    </v-row>
-    <v-row justify="center">
-      <v-btn x-large color="primary" ref="homeBtn" @click.native="goHome">
-        Go back
-      </v-btn>
-    </v-row>
+  <v-container fluid class="d-flex align-center justify-center">
+    <v-sheet
+      class="d-flex flex-column pa-10"
+      height="60%"
+      width="50%"
+      elevation="5"
+      rounded="lg"
+    >
+      <v-row justify="center">
+        <v-icon color="error" aria-hidden="true">
+          mdi-alert-circle-outline
+        </v-icon>
+      </v-row>
+      <v-row justify="center" align="start">
+        <h1 class="text-h1 onyx--text">Sorry,</h1>
+      </v-row>
+      <v-row justify="center" align="start">
+        <p class="text-h3 text-center onyx--text">that room doesn't exist!</p>
+      </v-row>
+      <v-row justify="center">
+        <v-btn x-large color="success" ref="homeBtn" @click.native="goHome">
+          Go back
+        </v-btn>
+      </v-row>
+    </v-sheet>
   </v-container>
 </template>
 
