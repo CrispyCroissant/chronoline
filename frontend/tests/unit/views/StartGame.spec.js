b98fd017d196fae754120314dd5ecb99401746a1
CrispyCroissant <gentlemug00@gmail.com>
2022-02-20 13:48:19 +0100

    Fixed lint error

--- frontend/tests/unit/views/StartGame.spec.js
+++ frontend/tests/unit/views/StartGame.spec.js
@@ -1,15 +1,13 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Vuetify from "vuetify";
-import VueRouter from "vue-router";
 import StartGame from "@/views/StartGame.vue";
 
 describe("The start game page", () => {
   const localVue = createLocalVue();
-  let vuetify, router;
+  let vuetify;
 
   beforeEach(() => {
     vuetify = new Vuetify();
-    router = new VueRouter();
   });
 
   it("exists", () => {
