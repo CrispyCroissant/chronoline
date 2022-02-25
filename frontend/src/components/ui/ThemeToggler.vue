7da9ee7018086c5be835209dc2f93443d4a10ce7
CrispyCroissant <48359983+CrispyCroissant@users.noreply.github.com>
2022-02-25 14:19:45 +0100

    Created theme toggler component

---
+++ frontend/src/components/ui/ThemeToggler.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-btn ref="btn" icon @click.native="toggleTheme">
+    <v-icon x-large :color="color">{{ icon }}</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "ThemeToggler",
+  computed: {
+    color() {
+      if (this.$vuetify.theme.dark) return "yellow";
+      return "blue";
+    },
+    icon() {
+      if (this.$vuetify.theme.dark) return "mdi-weather-sunny";
+      return "mdi-moon-waning-crescent";
+    },
+  },
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ frontend/tests/unit/components/ui/ThemeToggler.spec.js
@@ -0,0 +1,51 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Vuetify from "vuetify";
+import ThemeToggler from "@/components/ui/ThemeToggler.vue";
+
+describe("The theme toggler", () => {
+  const localVue = createLocalVue();
+  let vuetify, wrapper;
+
+  beforeEach(() => {
+    vuetify = new Vuetify({
+      theme: {
+        dark: true,
+      },
+    });
+
+    wrapper = shallowMount(ThemeToggler, {
+      localVue,
+      vuetify,
+    });
+  });
+
+  it("exists", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("toggles theme on button click", async () => {
+    const spy = jest.spyOn(wrapper.vm, "toggleTheme");
+
+    await wrapper.findComponent({ ref: "btn" }).trigger("click");
+
+    expect(spy).toBeCalledTimes(1);
+  });
+
+  it("is yellow if dark mode is enabled", () => {
+    expect(wrapper.vm.color).toBe("yellow");
+  });
+
+  it("is blue if light mode is enabled", () => {
+    wrapper.vm.$vuetify.theme.dark = false;
+    expect(wrapper.vm.color).toBe("blue");
+  });
+
+  it("changes the icon to a sun if dark mode is enabled", () => {
+    expect(wrapper.vm.icon).toBe("mdi-weather-sunny");
+  });
+
+  it("changes the icon to a moon if light mode is enabled", () => {
+    wrapper.vm.$vuetify.theme.dark = false;
+    expect(wrapper.vm.icon).toBe("mdi-moon-waning-crescent");
+  });
+});
