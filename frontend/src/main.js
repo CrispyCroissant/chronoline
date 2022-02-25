d9e9ff88bd0f5d89f4b5c5927c0810d2f7d3ae73
CrispyCroissant <48359983+CrispyCroissant@users.noreply.github.com>
2022-02-25 17:03:04 +0100

    App uses meta package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,12 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VueSocketIOExt from "vue-socket.io-extended";
 import { io } from "socket.io-client";
+import VueMeta from "vue-meta";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueSocketIOExt, io(process.env.VUE_APP_BACKEND_URL));
+Vue.use(VueMeta);
 
 new Vue({
   router,
