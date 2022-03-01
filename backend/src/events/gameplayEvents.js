cbb7b1e71be03cf6a29348aa7919d1942eebf630
CrispyCroissant <48359983+CrispyCroissant@users.noreply.github.com>
2022-03-01 14:14:56 +0100

    Moved out of place comment

--- backend/src/events/gameplayEvents.js
+++ backend/src/events/gameplayEvents.js
@@ -22,14 +22,13 @@ function startGame(io, socket) {
 
   room.changeStatus();
 
+  // Fill the deck
   const playerAmount = room.players.length;
   if (playerAmount <= 5) {
     room.fillDeck(playerAmount * 8);
   } else {
     room.fillDeck(50);
   }
-
-  // Fill the deck
 
   // Give each player 5 unique cards.
   room.handOutCards(5);
